d5fd089f4def8c7a2736dad64bfe935aac7e79da
sakshi-shitole <sakshishitole1@gmail.com>
2023-09-14 16:08:09 +0530

    logo redirect to home page done successfully

--- src/components/Navbar/MovieNavbar.Component.jsx
+++ src/components/Navbar/MovieNavbar.Component.jsx
@@ -1,19 +1,31 @@
 import React, { useContext } from 'react';
 import { BiChevronDown, BiMenu, BiShareAlt, BiSearch } from 'react-icons/bi';
 import { MovieContext } from '../../context/Movie.context';
+import { Link } from 'react-router-dom';
 
 const NavSm = () => {
     const { movie } = useContext(MovieContext);
 
     return (<>
-        <div className='text-gray-700 flex items items-center justify-between'>
+        <div className='text-gray-700 w-full flex items items-center justify-between'>
+            <div className="w-10 h-10">
+                <Link to="/"><img src='https://i.ibb.co/zPBYW3H/imgbin-bookmyshow-office-android-ticket-png.png'
+                    alt='logo'
+                    className='w-full h-full'
+                /></Link>
+            </div>
             <div>
                 <h3 className='text-xl font-bold'>
                     {movie.original_title}
                 </h3>
             </div>
-            <div className='w-8 h-8'>
-                <BiShareAlt className='w-full h-full' />
+            <div className=' flex items-center justify-between'>
+                <div className='w-8 h-8'>
+                    <BiShareAlt className='w-full h-full' />
+                </div>
+                <div className='w-8 h-8'>
+                    <BiMenu className='w-full h-full' />
+                </div>
             </div>
         </div>
 
@@ -21,15 +33,55 @@ const NavSm = () => {
     );
 };
 
+function NavMd() {
+    return <>
+        <div className="container flex mx-auto px-4 items-center justify-between">
+            <div className='flex items-center w-1/2 gap-3'>
+                <div className=" w-10 h-10">
+                    <Link to="/"><img
+                        src="https://i.ibb.co/zPBYW3H/imgbin-bookmyshow-office-android-ticket-png.png"
+                        alt="logo"
+                        className='w-full h-full'
+                    />
+                    </Link>
+                </div>
+
+                <div className=" w-full flex items-centre gap-3 bg-white px-3 py-1 rounded-md ">
+                    <BiSearch className='w-6 h-6 ' />
+                    <input
+                        type="search"
+                        className='w-full bg-transparent border-none focus:outline-none'
+                        placeholder=" Search for movies, events, plays, sports and activities"
+                    />
+                </div>
+            </div>
+            <div className='flex items-center gap-3'>
+                <span className='text-gray-500 text-base flex items-center cursor-pointer hover:text-white'>
+                    Nagpur <BiChevronDown />
+                </span>
+                <Link to="/plays" className="text-gray-500 text-base flex items-center cursor-pointer hover:text-white">
+                    Plays
+                </Link>
+                <button className='bg-red-600 text-white px-2 py-1 text-sm rounded'>
+                    Sign In
+                </button>
+                <div className='w-8 h-8 text-gray-500'>
+                    <BiMenu className='w-full h-full' />
+                </div>
+            </div>
+        </div>
+    </>;
+}
+
 const NavLg = () => {
     return (<>
         <div className='container flex mx-auto px-4 items-center justify-between'>
             <div className='flex items-center w-1/2 gap-3'>
                 <div className="w-10 h-10">
-                    <img src='https://i.ibb.co/zPBYW3H/imgbin-bookmyshow-office-android-ticket-png.png'
+                    <Link to="/"><img src='https://i.ibb.co/zPBYW3H/imgbin-bookmyshow-office-android-ticket-png.png'
                         alt='logo'
                         className='w-full h-full'
-                    />
+                    /></Link>
                 </div>
                 <div className=" w-full flex items-center gap-3 bg-white px-3 py-2 rounded-md">
                     <BiSearch />
@@ -59,9 +111,13 @@ const NavLg = () => {
 const MovieNavbar = () => {
     return <>
         <nav className='bg-white border-b-2 lg:border-b-0 lg:bg-darkBackground-700 p-4'>
-            <div className='lg:hidden'>
+            <div className='md:hidden lg:hidden'>
                 {/*Mobile and medium Screens */}
                 <NavSm />
+            </div>
+            <div className='hidden sm:hidden w-full md:flex lg:hidden'>
+                {/*tablet and notes*/}
+                <NavMd />
             </div>
             <div className='hidden w-full lg:flex'>
                 {/*large Screens */}
--- src/components/Navbar/Navbar.Component.jsx
+++ src/components/Navbar/Navbar.Component.jsx
@@ -4,7 +4,15 @@ import { Link } from 'react-router-dom';
 
 function NavSm() {
     return <>
-        <div className="text-white flex items-centre justify-between">
+        <div className="text-white mx-auto px-4 flex  items-center justify-between">
+            <div className=" w-10 h-10 ">
+                <Link to="/"><img
+                    src="https://i.ibb.co/zPBYW3H/imgbin-bookmyshow-office-android-ticket-png.png"
+                    alt="logo"
+                    className='w-full h-full'
+                />
+                </Link>
+            </div>
             <div>
                 <h3 classname="text-xl font-bold">It All Starts Here!</h3>
                 <span className=" text-gray-400 text-xs flex 
@@ -14,35 +22,52 @@ function NavSm() {
                 <Link to="/plays" className="text-gray-400 text-xs flex items-centre cursor-pointer hover:text-white">
                     Plays
                 </Link>
-
             </div>
-            <div className="w-8 h-8">
+
+            <div className="w-8 h-8 flex last:">
                 <BiSearch className='w-full h-full' />
             </div>
+
         </div>
     </>;
 }
 
 function NavMd() {
     return <>
-        <div className='flex items-center w-full gap-3'>
-            <div className=" w-10 h-10">
-                <img
-                    src="https://i.ibb.co/zPBYW3H/imgbin-bookmyshow-office-android-ticket-png.png"
-                    alt="logo"
-                    className='w-full h-full'
-                />
-            </div>
-            <div className=" w-full flex items-centre gap-3 bg-white px-3 py-1 rounded-md">
-                <BiSearch className='w-6 h-6' />
-                <input
-                    type="search"
-                    className='w-full bg-transparent border-none focus:outline-none'
-                    placeholder=" Search for movies, events, plays, sports and activities"
-                />
+        <div className="container flex mx-auto px-4 items-center justify-between">
+            <div className='flex items-center w-1/2 gap-3'>
+                <div className=" w-10 h-10">
+                    <Link to="/"><img
+                        src="https://i.ibb.co/zPBYW3H/imgbin-bookmyshow-office-android-ticket-png.png"
+                        alt="logo"
+                        className='w-full h-full'
+                    />
+                    </Link>
+                </div>
 
+                <div className=" w-full flex items-centre gap-3 bg-white px-3 py-1 rounded-md">
+                    <BiSearch className='w-6 h-6' />
+                    <input
+                        type="search"
+                        className='w-full bg-transparent border-none focus:outline-none'
+                        placeholder=" Search for movies, events, plays, sports and activities"
+                    />
+                </div>
             </div>
-
+            <div className='flex items-center gap-3'>
+                <span className='text-gray-200 text-base flex items-center cursor-pointer hover:text-white'>
+                    Nagpur <BiChevronDown />
+                </span>
+                <Link to="/plays" className="text-gray-200 text-base flex items-center cursor-pointer hover:text-white">
+                    Plays
+                </Link>
+                <button className='bg-red-600 text-white px-2 py-1 text-sm rounded'>
+                    Sign In
+                </button>
+                <div className='w-8 h-8 text-white'>
+                    <BiMenu className='w-full h-full' />
+                </div>
+            </div>
         </div>
     </>;
 }
@@ -52,10 +77,10 @@ function NavLg() {
         <div className="container flex mx-auto px-4 items-center justify-between">
             <div className='flex items-center w-1/2 gap-3'>
                 <div className="w-10 h-10">
-                    <img src="https://i.ibb.co/zPBYW3H/imgbin-bookmyshow-office-android-ticket-png.png"
+                    <Link to="/"><img src="https://i.ibb.co/zPBYW3H/imgbin-bookmyshow-office-android-ticket-png.png"
                         alt="logo"
                         className=" w-full h-full"
-                    />
+                    /></Link>
                 </div>
                 <div className=" w-full flex items-center gap-3 bg-white px-3 py-1 rounded-md">
                     <BiSearch />
